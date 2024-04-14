d4c5013070f04428239ab77b5f32fa59ca62c53a
Wetterbro <47597724+Wetterbro@users.noreply.github.com>
2024-04-14 21:08:25 +0200

    Update page.jsx

--- src/app/productpage/page.jsx
+++ src/app/productpage/page.jsx
@@ -29,7 +29,7 @@ const Page = () => {
                         <Link href="./" className="order-2 md:order-none">
                             Back to Home
                         </Link>
-                        <h1 className="text-3xl font-bold p-4 order-1 md:order-none">DriscTrace</h1>
+                        <h1 className="text-3xl font-bold p-4 order-1 md:order-none">DiscTrace</h1>
                         <div className="w-full md:block md:w-auto order-3 md:order-none" id="navbar-default">
                             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                                 {['My Discs', 'Disc Lookup'].map((tabName) => (
