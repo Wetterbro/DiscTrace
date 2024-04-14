b34cf3b51000929c57b0da414a0d28690d9848b3
Wetterbro <47597724+Wetterbro@users.noreply.github.com>
2024-04-14 21:10:03 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -22,7 +22,7 @@ export default function Page() {
         <header>
           <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-col md:flex-row items-center justify-between mx-auto p-4">
-              <h1 className="text-3xl font-bold p-4 order-1 md:order-none">DriscTrace</h1>
+              <h1 className="text-3xl font-bold p-4 order-1 md:order-none">DiscTrace</h1>
               <div className="w-full md:block md:w-auto order-2 md:order-none" id="navbar-default">
                 <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                   {['product', 'contact'].map((tabName) => (
